b3bd0e689cf1fb82075c7a487f8c9814948cda2b
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2019-09-24 22:03:24 -0400

    Problem Sets 1 & 2 Complete

--- pset2.js
+++ pset2.js
@@ -1,9 +1,11 @@
 /******************
 PROBLEM 1:
 
-In this problem, you will be downloading some files from online, then creating a custom project with git initialized. You will then one by one structure the files and make commits to git as needed.
+In this problem, you will be downloading some files from online, then creating a custom project with git initialized. 
+You will then one by one structure the files and make commits to git as needed.
 
-1. Download this repository from the homepage by clicking "Clone or download", then "Download ZIP". You will be using the files from the /stuff directory.
+1. Download this repository from the homepage by clicking "Clone or download", then "Download ZIP". You will be using the files 
+from the /stuff directory.
 2. Once the repository has been downloaded, go ahead and unzip it.
 
 3. Achieve the following structure ONLY using command line:
